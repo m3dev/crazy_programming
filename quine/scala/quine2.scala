965186c055c5e007a5f082a004109019b58feff7
vaaaaanquish <47341845+vaaaaanquish@users.noreply.github.com>
2026-04-28 23:26:04 +0900

    add scala quine 2
    新規にScala Quineを追加

---
+++ quine/scala/quine2.scala
@@ -0,0 +1,26 @@
+//> using dep org.scala-lang::scala3-compiler // --------- We are hiring!! @m3_engineering ---------
+@main def f()=javax.script.ScriptEngineManager().getEngineByName("scala").eval("""import#java.util._
+,java.util.zip._,java.io._;val#d=Base64.getDecoder;var#s=new#String(d.decode("Ly8+IHVzaW5nIGRlcCBvcm
+cuc2NhbGEtbGFuZzo6c2NhbGEzLWNvbXBpbGVyIC8vIC0tLS0tLS0tLSBXZSBhcmUgaGlyaW5nISEgQG    0zX2VuZ2luZWVyaW
+5nIC0tLS0t             LS0tLUBtYWluIGRlZiBmKC            k9amF2YXguc2NyaXB0            LlNjcmlwdEVuZ
+2luZU1hbmFnZ           XIoKS5nZXRFbmdpbmVCeU          5hbWUoInNjYWxhIikuZ                XZhbCgiIiJp
+bXBvcnQjamF2YS5         1dGlsLl8samF2YS51dG         lsLnppcC5fLGphdmEua     W8uXzt        2YWwjZD1CY
+XNlNjQuZ2V0RGVj           b2Rlcjt2YXIjcz1u          ZXcjU3RyaW5nKGQuZGV   jb2RlKCIl       cyIpKTtzPX
+MuZm9ybWF0KEJhc           2U2NC5nZXRFbmNv           ZGVyLmVuY29kZVRvU3RyaW5nKHMuZ2V       0Qnl0ZXMoK
+SkpO3ZhciNpPTA7            cHJpbnQobmV3I            1N0cmluZyhuZXcjSW5mbGF0ZXJJbnB1      dFN0cmVhbSh
+uZXcjQnl0ZUFycm             F5SW5wdXRTdH            JlYW0oZC5kZWNvZGUoImVKelZsbEVP      eENBSVJQODVE
+ZDcvY3J0Sm15Z3d    B         OVJzdXlsZk    Ns       TWVnaVlkNDM2VEJ4Z3ZoZWpYRW        9pdXhqTndCWTBL
+VVJXTW9nQW44ZTF    xc        FdFeEsydE    FZc        XJxSEY2NEF5a0h0Z1hSWG9C            QkZrY0dVUmE0
+eUlBUU5PRmpWME    p3R1        JBU3RlZ     W1P        QW83clVRWnFFZ2JFUFZX                 UW16SDdLSW
+FTcVF3eUNxZFgv    UW1IM         lFVT     XBXT        HZnbnhGMFJDMEplanlQa3JDSHppUF         VqZUx2czl
+1Q2NNWWwzNTREV    VhVd2l         zW     kRyRV        Jhd0FiRkFUWTRnOFJpenhhNXFIUjhaY        mZ5VCtCd
+mtBZ3BMd0ZBPT0    iKSkpLn        J     lYWRBb        GxCeXRlcygpKS5tYXB7Y2FzZScxJz0+a       Ss9MTtzK
+GktMSk7Y2FzZS    cwJz0+Jy             MnO2Nhc2        UjeD0+eH0ubWtTdHJpbmcpIiIiLnJlc       GxhY2UoI
+lxuIiwiIikucm    VwbGFjZSg           iICIsIiIp        LnJlcGxhY2UoIiMiLCIgIikp"));s=        s.format
+(Base64.getEn    coder.enco         deToString        (s.getBytes()));   var#i=0;pri       nt(new#St
+ring(new#Infl    aterInputSt       ream(new#By        teArrayInputStr    eam(d.deco       de("eJzVll
+EOxCAIRP              85Dd7/c      rtJmygw                 A9RsuylfCl                   MegiYd436TBx
+gvhejXEo              iuxjNwBY    0KURWMog                 An8e1qpWExK2              tAYqrqHF64AykHt
+gXRXoBBFkcGURa4yIAQNOFjV0JwGRASteemOAo7rUQZqEgbEPVWQmzH7KIaSqQwyCqdX/QmH2QUMpWLvgnxF0RC0JejyPkrCHziP
+UjeLvs9uCcMYl354DUXUwisZDrERawAbFATY4g8Rizxa5qHR8ZbfyT+BvkAgpLwFA=="))).readAllBytes()).map{case'1'=
+>i+=1;s(i-1);case'0'=>'#';case#x=>x}.mkString)""".replace("\n","").replace(" ","").replace("#"," "))
